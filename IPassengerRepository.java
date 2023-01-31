59e53ee7536a907c930f3cb4226aeca525b80c72
FlightCapG <123924266+FlightCapG@users.noreply.github.com>
2023-01-31 21:20:43 +0530

    Add files via upload

---
+++ IPassengerRepository.java
@@ -0,0 +1,15 @@
+package com.cg.flight.app.repository;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.flight.app.entity.Passenger;
+
+@Repository
+public interface IPassengerRepository extends JpaRepository<Passenger, BigInteger> {
+	
+}
+
