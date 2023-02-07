f9c79a6e6dfd7222c8a30100202c006c059a5156
FlightCapG <123924266+FlightCapG@users.noreply.github.com>
2023-02-07 18:58:00 +0530

    Add files via upload

---
+++ FlightManagementSystem/src/main/java/com/cg/flight/app/dto/PassengerResponseDTO.java
@@ -0,0 +1,11 @@
+package com.cg.flight.app.dto;
+
+import lombok.Data;
+@Data
+public class PassengerResponseDTO {
+private String msg;
+private int prnNumber;
+private String passengerName;
+private int passengerAge;
+private String city;
+}
